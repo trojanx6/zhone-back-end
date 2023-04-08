29c3fad7d24a37912144aa71025df5d93b0a3364
SpyEye <86468725+trojanx6@users.noreply.github.com>
2023-04-08 20:17:53 +0300

    Add files via upload

---
+++ zhone.py
@@ -0,0 +1,116 @@
+
+__AUTHOR__="Naci Caner"
+
+import requests as req
+from bs4 import BeautifulSoup as btu
+from typing import Union
+import logging
+import datetime
+from sys import exit as close_progam
+import re
+import json
+
+class Zhone_Info:
+    def __init__(self) -> None:
+        self.domain = input("Domain:").strip()
+        self.url = f"https://check-host.net/ip-info?host={self.domain}"
+        self.headers = {r"User-Agnet":"Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12H321 [FBAN/FBIOS;FBAV/38.0.0.6.79;FBBV/14316658;FBDV/iPad4,1;FBMD/iPad;FBSN/iPhone OS;FBSV/8.4.1;FBSS/2; FBCR/;FBID/tablet;FBLC/en_US;FBOP/1"}
+        logging.basicConfig(filename="back.log",
+                            format='%(asctime)s %(message)s',
+                            filemode='w')
+        self.log = logging.getLogger()
+        self.log.setLevel(logging.DEBUG)
+        self.log.info(f"{datetime.datetime.now()} - The Program was run")
+        self.log.info(f"Host Entered, Entered Host Name:{self.url}")
+
+        self.__zone__(self.url)
+
+
+    def __zone__(self, url:Union[bytes | str]) -> str:
+
+        """
+        Gets Host Status for Entered Site
+
+
+        :parameter str(host,bytes)
+        :return: json format
+
+        Explanation:
+        39: Submits a GET Request to the Site with the Domain Name in
+        30-54: codes in this line range: Analyzes the status codes in Zone_Requests variable and writes it to the log file
+        57-62 Line: Section of Tables Containing Classes with Information Related to IP Addresses
+        65:I wrote this line because there are too many spaces in the incoming data:be warned though this removes "all whitespace characters (space, tab, newline, return, form feed)" . In, "this is \t a test\n" will effectively end up as "this is a test"
+        66-78:The incoming data string was somehow plain. I extracted the plain data with the re module. I pulled the necessary information.
+        81-96:In order to make the incoming data json, I first converted it to dictionaries, then I converted it to json data with "json.dumps", as a result, the data is now Json
+        """
+        Zone_Requests = req.get(url=url, headers=self.headers)
+        match Zone_Requests.status_code:
+            case 200:
+               self.log.info("200 Successful")
+            case 404:
+               self.log.critical("The Requested Page cannot Be Found")
+               close_progam()
+            case 400:
+               self.log.critical("The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing)")
+               close_progam()
+            case 500:
+               self.log.critical("The server has encountered a situation it does not know how to handle.")
+               close_progam()
+            case 504:
+               self.log.critical("This error response is given when the server is acting as a gateway and cannot get a response in time.")
+               close_progam()
+
+
+        Zone_Html = Zone_Requests.text
+        Zone_Soup = btu(Zone_Html,"html.parser")
+        Div_incontent = Zone_Soup.find_all("div",id="incontent")
+        for i in Div_incontent:
+            Div_ipinfo = i.find("div",id="ip_info-dbip")
+            Tbody = Div_ipinfo.find("table").text
+
+        text = " ".join(Tbody.split())
+        ip_address = re.search(r"IP address (\d+\.\d+\.\d+\.\d+)", text).group(1)
+        host_name = re.search(r"Host name (\d+\.\d+\.\d+\.\d+)", text).group(1)
+        ip_range = re.search(r"IP range (\d+\.\d+\.\d+\.\d+-\d+\.\d+\.\d+\.\d+)", text).group(1)
+        isp = re.search(r"ISP (.+?) Organization", text).group(1)
+        organization = re.search(r"Organization (.+?) Country", text).group(1)
+        country = re.search(r"Country (.+?) Region", text).group(1)
+        region = re.search(r"Region (.+?) City", text).group(1)
+        city = re.search(r"City (.+?) Time zone", text).group(1)
+        time_zone = re.search(r"Time zone (.+?) Local time", text).group(1)
+        local_time = re.search(r"Local time (.+?) Postal Code", text).group(1)
+        postal_code = re.search(r"Postal Code (.+?) Powered by", text).group(1)
+        powered_by = re.search(r"Powered by (.+)", text).group(1)
+
+        data = {
+            "IP address": ip_address,
+            "Host name": host_name,
+            "IP range": ip_range,
+            "CIDR": "N/A",
+            "ISP": isp,
+            "Organization": organization,
+            "Country": country,
+            "Region": region,
+            "City": city,
+            "Time zone": time_zone,
+            "Local time": local_time,
+            "Postal Code": postal_code,
+            "Powered by": powered_by
+        }
+        return_json = json.dumps(data)
+        print(return_json)
+
+
+if __name__=="__main__":
+    ip = Zhone_Info()
+
+
+
+
+
+
+
+
+
+
+
